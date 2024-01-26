c7b24d62dc5073e010c2d494fc1e58451cd6997d
DavronDostqobilov <dostqobilovdavron885@gamil.com>
2024-01-26 10:20:27 +0500

    new1

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ app=Flask(__name__)
 @app.route('/')
 def info():
     return "Salom Davronshox"
-
+@app.route('/davron')
+def about():
+    return "My name is Davron,My lastname is Do'stqobilov.\nI am 20 years old. I study in SamTATU"
 if __name__=="__main__":
     app.run()
